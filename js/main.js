b9a3ad7fd1b4236de776d156de2e1bafeafb3a63
Davide <rinaldi.davide94@gmail.com>
2020-12-10 11:49:50 +0100

    init vuejs

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const myApp = new Vue({
+	el: "#root",
+	data: {
+        linkHeader: [
+            {
+                name: 'Home',
+                link: '#'
+            },
+            {
+                name: 'Rates',
+                link: '#'
+            },
+            {
+                name: 'Testimonials',
+                link: '#'
+            },
+            {
+                name: 'FAQ',
+                link: '#'
+            },
+            {
+                name: 'Blog',
+                link: '#'
+            },
+            {
+                name: 'Contact',
+                link: '#'
+            },
+        ],
+        linkFooterPost: [
+            {
+                name: 'Heading Out To College?',
+                link: '#'
+            },
+            {
+                name: 'Moving Your Business?',
+                link: '#'
+            },
+            {
+                name: 'Outstading Quality',
+                link: '#'
+            },
+            {
+                name: 'Cost of Moving',
+                link: '#'
+            },
+            {
+                name: 'Best Moving Tips',
+                link: '#'
+            },
+        ]
+    }
+
+
+})
